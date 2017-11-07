a2f78f04ad42ba22ab2ff643ab4d0b2060817f0e
Salman Zafar <salmanzafar949@gmail.com>
2017-11-07 13:31:19 +0500

    Insta Connection via curl and get tokens and printing username

--- index.php
+++ index.php
@@ -12,7 +12,29 @@ include 'InstaApi.php';
 if(isset($_GET['code'])) {
     $code = $_GET['code'];
     if (!empty($code)) {
-        echo $code;
+
+        $url ="https://www.instagram.com/oauth/access_token";
+        $access_token_settings=array(
+            'client_id' =>  client_id,
+            'client_secret' =>  client_secret,
+            'grant_type'   => 'authorization_code',
+            'redirect_uri' => redirect_uri,
+            'code'  => $code
+        );
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,false);
+        $res = curl_exec($curl);
+        curl_close($curl);
+
+        $user_data = json_decode($res,true);
+        $username = $user_data['user']['name'];
+        echo $username;
+
+
+
     }
 }
 else
